4627545368cd8c16cfa468b264c483dc0ff91241
GautamMakadia <gautammakadia12345@gmail.com>
2021-12-14 19:11:15 +0530

    Data Caching Support

--- app/src/main/java/com/gs/VeterinaryCare/VeterinaryApplication.java
+++ app/src/main/java/com/gs/VeterinaryCare/VeterinaryApplication.java
@@ -3,12 +3,17 @@ package com.gs.VeterinaryCare;
 import android.app.Application;
 
 import com.google.android.material.color.DynamicColors;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class VeterinaryApplication extends Application {
     @Override
     public void onCreate() {
-        DynamicColors.applyToActivitiesIfAvailable(this);
         super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
+        DatabaseReference AnimalRef = FirebaseDatabase.getInstance().getReference("Animals");
+        AnimalRef.keepSynced(true);
     }
 }
