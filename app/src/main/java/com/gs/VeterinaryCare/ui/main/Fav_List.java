16adc8f6c72d2a5653febfce601ccfd0e98af6e8
GautamMakadia <gautammakadia12345@gmail.com>
2021-12-16 18:39:22 +0530

    code cleanup

--- app/src/main/java/com/gs/VeterinaryCare/ui/main/Fav_List.java
+++ app/src/main/java/com/gs/VeterinaryCare/ui/main/Fav_List.java
@@ -1,12 +1,10 @@
 package com.gs.VeterinaryCare.ui.main;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Toolbar;
-
 import com.google.android.material.appbar.MaterialToolbar;
-import com.gs.VeterinaryCare.R;
 import com.gs.VeterinaryCare.databinding.ActivityFavListBinding;
 
 public class Fav_List extends AppCompatActivity {
@@ -20,9 +18,7 @@ public class Fav_List extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         MaterialToolbar materialToolbar = binding.topAppToolBar;
-        materialToolbar.setNavigationOnClickListener(view -> {
-            finish();
-        });
+        materialToolbar.setNavigationOnClickListener(view -> finish());
     }
 
 }
