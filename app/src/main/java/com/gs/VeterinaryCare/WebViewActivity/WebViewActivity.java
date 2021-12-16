04633d14cf120145775f082a4a2d7827771d04d2
GautamMakadia <gautammakadia12345@gmail.com>
2021-12-16 16:12:54 +0530

    Fixed : WebView Displays Old State Of Page When Page Content Is Changed

--- app/src/main/java/com/gs/VeterinaryCare/WebViewActivity/WebViewActivity.java
+++ app/src/main/java/com/gs/VeterinaryCare/WebViewActivity/WebViewActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.gs.VeterinaryCare.databinding.WebViewActivityBinding;
@@ -22,15 +23,19 @@ public class WebViewActivity extends AppCompatActivity {
 
         MaterialToolbar materialToolbar = binding.topAppToolBar;
         setSupportActionBar(materialToolbar);
-
-        materialToolbar.setNavigationOnClickListener(view -> {
-            finish();
-        });
-
         materialToolbar.setTitle(getIntent().getStringExtra("AnimalName"));
 
         webView = binding.webView;
         webView.loadUrl(getIntent().getStringExtra("PageURL"));
 
+        materialToolbar.setNavigationOnClickListener(view -> {
+            finish();
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        webView.reload();
     }
 }
