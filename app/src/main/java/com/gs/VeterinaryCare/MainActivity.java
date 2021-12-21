0b8dd6264c66976f2a2b2c83feabcfecb3960753
GautamMakadia <gautammakadia12345@gmail.com>
2021-12-21 17:55:58 +0530

    On SignOut() Doesn't updateUI To SignedOut State Without Activity Restart

--- app/src/main/java/com/gs/VeterinaryCare/MainActivity.java
+++ app/src/main/java/com/gs/VeterinaryCare/MainActivity.java
@@ -212,8 +212,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Method To SignOut User From App.
     private void signOut() {
-        mGoogleSignInClient.signOut().addOnCompleteListener(OnCompleteListener->{
+        mGoogleSignInClient.signOut().addOnCompleteListener(this, task ->{
             isUsrLoggedIn = false;
+            account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
             updateUI(account);
         });
     }
